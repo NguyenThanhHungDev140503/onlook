126be32a26cfbd9ddf3b68a880725d4a0220150a
Nguyen Thanh Hung <nthungdev.140503@gmail.com>
2026-08-18 19:48:28 +0700

    fix(web): remove broken Zaraz script

--- apps/web/client/src/app/layout.tsx
+++ apps/web/client/src/app/layout.tsx
@@ -11,7 +11,6 @@ import { type Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale } from 'next-intl/server';
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import { ThemeProvider } from './_components/theme';
 import { AuthProvider } from './auth/auth-context';
 import { faqSchema, organizationSchema } from './seo';
@@ -74,7 +73,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             <body>
                 {isProduction && (
                     <>
-                        <Script src="https://z.onlook.com/cdn-cgi/zaraz/i.js" strategy="lazyOnload" />
                         <RB2BLoader />
                     </>
                 )}
